ed3700903aa63a35e8ff9b4c50d13822858c4476
Hayato Kajiyama <kaji1216@gmail.com>
2014-04-09 00:50:15 +0900

    完全にパース完了

--- univcal.py
+++ univcal.py
@@ -2,19 +2,24 @@
 import unicodedata
 from bs4 import BeautifulSoup
 
+
+def convertZenhan(html):
+    s = html.read().decode('utf-8')
+    html = unicodedata.normalize('NFKC', s).encode('utf-8')
+    return html
+
 html = open('plan.html', 'r')
-s = html.read().decode('utf-8')
-html = unicodedata.normalize('NFKC', s).encode('utf-8')
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(convertZenhan(html))
 
 f = open('test.html', 'w')
 
 # 適宜変更
 source = soup.find_all('table', border="1")
 
-for x in source:
-    for y in x("tr"):
-        print(y)
-
-
-f.write(str(source))
+for table in source:
+    for tr in table("tr"):
+        for cnt, td in enumerate(tr.stripped_strings):
+            if cnt == 0:
+                print(td.encode('utf-8'))
+            if cnt == 1:
+                print(td.encode('utf-8'))
