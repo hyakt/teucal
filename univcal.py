3873f9e039163d822ed8286572a2707c35564b5d
Hayato Kajiyama <kaji1216@gmail.com>
2014-04-08 19:47:26 +0900

    とりあえずHTMLパースまではできた

--- univcal.py
+++ univcal.py
@@ -1,8 +1,20 @@
 #-*- coding: utf-8 -*-
+import unicodedata
+from bs4 import BeautifulSoup
 
-from BeautifulSoup import BeautifulSoup
-
-html = open('./plan.html')
+html = open('plan.html', 'r')
+s = html.read().decode('utf-8')
+html = unicodedata.normalize('NFKC', s).encode('utf-8')
 soup = BeautifulSoup(html)
 
+f = open('test.html', 'w')
 
+# 適宜変更
+source = soup.find_all('table', border="1")
+
+for x in source:
+    for y in x("tr"):
+        print(y)
+
+
+f.write(str(source))
