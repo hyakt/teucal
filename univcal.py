4ee609e1ea1cd8acf1e04cc5a896ae13338246de
Hayato Kajiyama <kaji1216@gmail.com>
2014-04-11 16:51:23 +0900

    README.mdの編集 コードの最適化

--- univcal.py
+++ univcal.py
@@ -25,12 +25,11 @@ def getUnivCal():
     return html
 
 
-# htmlの整形
+# htmlの処理、記録
 def soupUnivCal(html):
-    tyear = date.today().strftime('%y')
-    nyear = str(int(tyear) + 1)
+    year = date.today().strftime('%y')
+    nyear = str(int(year) + 1)
     records = [['Subject', 'Start Date', 'End Date', 'All Day Event']]
-    nyearflag = False
 
     soup = BeautifulSoup(html)
     # 適宜変更
@@ -43,35 +42,22 @@ def soupUnivCal(html):
                 if cnt == 1:
                     days = re.findall(r'(\d+)月(\d+|\s\d+)日', td)
                     daycnt = len(days)
+                    # １月になったら翌年にする
                     if daycnt != 0:
                         if int(days[0][0]) == 1:
-                            nyearflag = True
-                    if nyearflag:
-                        if daycnt == 1:
-                            srtdate = days[
-                                0][0] + '/' + days[0][1].strip() + '/' + nyear
-                            tmplist.extend(
-                                [srtdate, '', 'True'])
-                        elif daycnt == 2:
-                            srtdate = days[
-                                0][0] + '/' + days[0][1].strip() + '/' + nyear
-                            enddate = days[
-                                1][0] + '/' + days[1][1].strip() + '/' + nyear
-                            tmplist.extend(
-                                [srtdate, enddate, 'True'])
-                    else:
-                        if daycnt == 1:
-                            srtdate = days[
-                                0][0] + '/' + days[0][1].strip() + '/' + tyear
-                            tmplist.extend(
-                                [srtdate, '', 'True'])
-                        elif daycnt == 2:
-                            srtdate = days[
-                                0][0] + '/' + days[0][1].strip() + '/' + tyear
-                            enddate = days[
-                                1][0] + '/' + days[1][1].strip() + '/' + tyear
-                            tmplist.extend(
-                                [srtdate, enddate, 'True'])
+                            year = nyear
+                    if daycnt == 1:
+                        srtdate = days[
+                            0][0] + '/' + days[0][1].strip() + '/' + year
+                        tmplist.extend(
+                            [srtdate, '', 'True'])
+                    elif daycnt == 2:
+                        srtdate = days[
+                            0][0] + '/' + days[0][1].strip() + '/' + year
+                        enddate = days[
+                            1][0] + '/' + days[1][1].strip() + '/' + year
+                        tmplist.extend(
+                            [srtdate, enddate, 'True'])
                 if cnt == 0:
                     tmplist.insert(0, td)
             records.append(tmplist)
